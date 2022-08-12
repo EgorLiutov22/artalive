93e82fed81ec37fa49f07a8ee9dce78969a31791
EgorLiutov22 <93601126+EgorLiutov22@users.noreply.github.com>
2022-08-12 22:53:49 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php wp_footer(); ?>
+<footer class="pt-5 my-5 text-muted text-center border-top">
+    Artalive.ru &middot; &copy; 2022
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="icon" type="image/png" sizes="192x192" href="penelope.svg">
+<title>Artalive</title>
+<link rel="stylesheet" href="bootstrap.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+<body>
+<img src="logo.jpg" class="rounded mx-auto d-block" width="810" height="142" alt="...">
+	<div class="container">
+		<h2 class="text-center fs-3">Творческая мастерская <strong>ArtAlive</strong></h2>
+
+		<p class="text-center fw-lighter text-muted fs-6">интерьерные картины, мастер классы</p>
+		<p class="text-center fs-5">Меня зовут Земфира, я художник и дизайнер интерьера.<br> Создала творческую мастерскую,<br> где вы можете заказать интерьерные картины для вашего дома или офиса.<br> Пройти обучающие мастер классы.</p>
+
+		<p class="text-center fs-6 fw-lighter">Работаем по будням с 11:00 до 21:00</p>
+	</div>
+	<div class="container">
+		<img src="1.jpg" class="rounded mx-auto scale animation d-block pb-5" width="630" height="455"  alt="...">
+		
+		<img src="2.jpg" class="rounded mx-auto d-block pb-5" width="630" height="455" alt="...">
+	</div>
+	<div class="container pb-5">
+		<p class="text-center  text-muted">Категории</p>
+		<div class="d-grid gap-2 col-6 mx-auto">
+		  <button class="btn btn-primary col-12 center-block mx-auto" href="#" type="button">Интерьерные картины</button>
+		  <button class="btn btn-primary col-12 center-block mx-auto" href="#" type="button">Обучение и мастер классы</button>
+		  <button class="btn btn-primary col-12 center-block mx-auto" href="#" type="button">Заказать дизайн проект</button>
+		  <button class="btn btn-primary col-12 center-block mx-auto" href="#" type="button">Заказать ремонт квартиры</button>
+		</div>
+	</div>
+	<div class="container">
+		<p class="text-center fs-4">Контакты</p>
+		<p class="text-center fw-normal text-muted">г. Уфа, Ленина 26, остановка Гостиный двор</p>
+		<p class="text-center phone fw-light text-reset"><a href="tel:+79191424020">+79191424020</a></p>
+		<p class="text-center fs-4">Как нас найти</p>
+		<p class="text-center fw-normal text-muted">Ленина 26, остановка Гостиный двор, г. Уфа <br> Вход в Башпотребсоюз</p>
+	</div>
+	<div class="container">
+		<div class="d-grid gap-2 col-6 mx-auto">
+	<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9b94845ff31a9eda03d150f532be832df2d463c8b8174c93f147e4dfccb99eed&amp;width=650&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+		</div>
+	</div>
+	<img src="final.svg" class="rounded mx-auto d-block" width="100" height="100" alt="...">
+	
+	<?php get_footer(); ?>
